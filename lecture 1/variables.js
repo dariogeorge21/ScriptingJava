8a6dcd623871d23c47a5710114b4e6a71c645d53
Dario George <edu.dariogeorge21@gmail.com>
2025-06-04 19:20:53 +0530

    chai aur code

--- lecture 1/variables.js
+++ lecture 1/variables.js
@@ -3,5 +3,7 @@ let account= "dariogeorge21@gmail.com";
 var password = "123456";
 city = "New Delhi";
 
+//prefer not to use var 
+
 console.table([number,account,password,city]);
 console.log(number);
