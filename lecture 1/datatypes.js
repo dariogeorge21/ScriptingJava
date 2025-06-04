7d3d84382a85ede9adf40491430427d3e5efba5e
Dario George <edu.dariogeorge21@gmail.com>
2025-06-04 19:45:24 +0530

    datatypes

---
+++ lecture 1/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all Js code as newer version
+
+alert("Helllo!!");//does not work in nodejs only in browser
+
+let name = "Dario"
+let age = 18;
+let loggedIn = true;
+
+// Datatypes
+/*
+number => limit 2^53
+bigint => for big number
+string " "
+boolean true/false
+null - standalone value
+undefined 
+symbol - unique
+*/
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined 
