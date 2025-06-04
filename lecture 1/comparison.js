4caf059be63b3f5c6286e366be6b13cf0d3d1a23
Dario George <edu.dariogeorge21@gmail.com>
2025-06-04 23:09:02 +0530

    comparee

---
+++ lecture 1/comparison.js
@@ -0,0 +1,11 @@
+console.log(null >= 0); //here when relational operators are used, the null is compared as a numerical value TRUE
+
+console.log(null==undefined)// true 
+
+console.log(null==0) // here it is not a relational operator therefore null is not treated as a 0 //false
+
+console.log(undefined==0)// false
+
+console.log(undefined>=0)// false
+
+console.log(undefined<0)//false
