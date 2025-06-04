91ea356aa9afa0ca6a01aea5607e101f600a7e6b
Dario George <edu.dariogeorge21@gmail.com>
2025-06-04 19:57:56 +0530

    conversion

---
+++ lecture 1/conversion.js
@@ -0,0 +1,19 @@
+let score="e";
+let val=Number(score);
+
+console.log(typeof val);
+console.log(val);
+
+//Here conversion takes place; if purely a number then converts to number; if alphabet+number then converts 
+// datatype => number ; value=>NaN
+
+// "33"=>33
+// "33e"=>NaN
+// true=>1;false=>0;
+
+let bool=true;
+let value=Boolean(bool);
+console.log(value);//1 if false then 0
+
+//If "" then false
+// if "dario" then true
