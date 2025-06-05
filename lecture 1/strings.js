0eb85453da7b75298b27ebe59707ed74938c5ebd
Dario George <edu.dariogeorge21@gmail.com>
2025-06-05 12:45:50 +0530

    string

---
+++ lecture 1/strings.js
@@ -0,0 +1,14 @@
+let course="Computer Science "
+let repoCount=55
+
+console.log(course+repoCount+" Timeline:4Yrs")// Concatenation - Outdated
+
+console.log(`Hello my course is ${course} and my repository count is ${repoCount}`)
+
+// Another method of declaring a string
+
+const name = new String ("Dario");
+console.log("Uppercase: ",name.toUpperCase());// copy converts to uppercase
+console.log("Lowercase: ",name.toLowerCase());// copy converts to lowercase
+console.log(name.charAt(0));//character at index =>0
+console.log(name.indexOf('a'))
