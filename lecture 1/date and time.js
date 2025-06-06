9722e46720358ced6e7490f5d678ba8707b6c631
Dario George <edu.dariogeorge21@gmail.com>
2025-06-06 23:10:06 +0530

    date & time

--- lecture 1/date and time.js
+++ lecture 1/date and time.js
@@ -1,8 +1,8 @@
 let myDate = new Date();
 
-console.log(myDate.toString())
-console.log(myDate.toLocaleString())
-console.log(typeof myDate)
+console.log(myDate.toString())// string conversion
+console.log(myDate.toLocaleString())// conversion for readability
+console.log(typeof myDate)// type => object
 
 let bday = new Date(2007,2,20) // get a specific date
 console.log(bday.toLocaleString()) 
