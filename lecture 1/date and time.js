8af8cb864952725967b90515908660b208246535
Dario George <edu.dariogeorge21@gmail.com>
2025-06-06 23:19:52 +0530

    date & time

--- lecture 1/date and time.js
+++ lecture 1/date and time.js
@@ -8,3 +8,10 @@ let bday = new Date(2007,2,20,11,30) // get a specific date and time
 console.log(bday.toLocaleString()) 
 
 console.log(bday.getTime());// to get miliseconds from the default 1970 time
+
+console.log(Date.now())// usually gives the number of miliseconds
+
+console.log(Math.floor(Date.now()/1000))// gives in seconds
+
+console.log(myDate.getMonth()+1)// starts from 0 month as array
+console.log(myDate.getDay())// starts from monday
