8b69a0373017dfa8137a64d0ccc351c6d0fb2330
Dario George <edu.dariogeorge21@gmail.com>
2025-06-06 23:14:51 +0530

    date & time

--- lecture 1/date and time.js
+++ lecture 1/date and time.js
@@ -4,5 +4,7 @@ console.log(myDate.toString())// string conversion
 console.log(myDate.toLocaleString())// conversion for readability
 console.log(typeof myDate)// type => object
 
-let bday = new Date(2007,2,20) // get a specific date
+let bday = new Date(2007,2,20,11,30) // get a specific date and time
 console.log(bday.toLocaleString()) 
+
+console.log(bday.getTime());// to get miliseconds from the default 1970 time
