87c2b19a6ae2ff40e62cebfc6cac7fbea59d35e2
Dario George <edu.dariogeorge21@gmail.com>
2025-06-10 23:38:23 +0530

    objects

---
+++ lecture 2/objects.js
@@ -0,0 +1,13 @@
+let myData = {
+    name: "Dario",
+    age: 18,
+    isStudent: true,
+    subjects: ["Math", "Computer Science", "Physics"],
+    address: {
+        street: "A-121, East Street",
+        city: "New Delhi",
+        country: "India"
+    }
+};
+console.log(myData.name); // Accessing a property
+console.log(myData);
