398294e0b882f492f4e1caec4bbec31eb95d92f4
Dario George <edu.dariogeorge21@gmail.com>
2025-06-10 23:05:23 +0530

    equality

---
+++ lecture 2/relational.js
@@ -0,0 +1,14 @@
+let num = 5;
+
+if (num> 3) {
+    console.log("num is greater than 3"); // this will execute
+}
+if (num < 10) {
+    console.log("num is less than 10"); // this will execute
+}
+if (num >= 5) {
+    console.log("num is greater than or equal to 5"); // this will execute
+}
+if (num <= 5) {
+    console.log("num is less than or equal to 5"); // this will execute
+}
