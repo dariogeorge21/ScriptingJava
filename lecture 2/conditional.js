e166ef88d6ab6e6215d48ebf35926b9b4f9f01af
Dario George <edu.dariogeorge21@gmail.com>
2025-06-10 23:35:23 +0530

    switch

---
+++ lecture 2/conditional.js
@@ -0,0 +1,9 @@
+let num = 5;
+
+if (num>0){
+    console.log("low")
+} else if (num > 10){
+    console.log("medium")
+} else {
+    console.log("high") 
+}
---
+++ lecture 2/switch.js
@@ -0,0 +1,21 @@
+let num = 5;
+switch (num) {
+    case 1:
+        console.log("num is 1");
+        break;
+    case 2:
+        console.log("num is 2");
+        break;
+    case 3:
+        console.log("num is 3");
+        break;
+    case 4:
+        console.log("num is 4");
+        break;
+    case 5:
+        console.log("num is 5"); // this will execute
+        break;
+    default:
+        console.log("num is not between 1 and 5");
+        break;
+}
